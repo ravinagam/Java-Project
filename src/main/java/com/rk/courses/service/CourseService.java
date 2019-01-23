13295a3282c42126a68cd589d3510dfc406bdf78
ravinagam <ravi.nagam.kiran@gmail.com>
2019-01-23 17:25:28 +0530

    fixed issue

--- src/main/java/com/rk/courses/service/CourseService.java
+++ src/main/java/com/rk/courses/service/CourseService.java
@@ -24,7 +24,7 @@ public class CourseService {
 
     public Course getCourseDetails(String name)
     {
-        return courseDAO.getCourseDetails(names);
+        return courseDAO.getCourseDetails(name);
     }
 
 }
