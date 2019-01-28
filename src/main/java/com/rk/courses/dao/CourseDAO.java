c8051f85d1ac688c126b45ee9912245eb9558d8f
ravinagam <ravi.nagam.kiran@gmail.com>
2019-01-28 11:36:34 +0530

    Updated AIFee

--- src/main/java/com/rk/courses/dao/CourseDAO.java
+++ src/main/java/com/rk/courses/dao/CourseDAO.java
@@ -16,7 +16,7 @@ public class CourseDAO {
 
     @Autowired
     Course course;
-    int courseFeeAI = 22500;
+    int courseFeeAI = 23000;
     int courseFeeML = 20000;
     public List<Course> getAllCourseDetails()
     {
