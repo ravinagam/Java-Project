6c94adbc78f9de6cc84f0b906aee971cd0065b22
ravinagam <ravi.nagam.kiran@gmail.com>
2019-03-19 12:11:54 +0530

    updated MLFee on 19Mar

--- src/main/java/com/rk/courses/dao/CourseDAO.java
+++ src/main/java/com/rk/courses/dao/CourseDAO.java
@@ -17,7 +17,7 @@ public class CourseDAO {
     @Autowired
     Course course;
     int courseFeeAI = 23000;
-    int courseFeeML = 21000;
+    int courseFeeML = 22000;
     public List<Course> getAllCourseDetails()
     {
         List<Course> courseList = new ArrayList<Course>();
