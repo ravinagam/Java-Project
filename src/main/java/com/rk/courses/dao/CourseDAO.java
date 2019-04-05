e0f8e62c7fbfced1e5535445f2bb389512eaec3d
ravinagam <ravi.nagam.kiran@gmail.com>
2019-04-05 12:20:02 +0530

    test

--- src/main/java/com/rk/courses/dao/CourseDAO.java
+++ src/main/java/com/rk/courses/dao/CourseDAO.java
@@ -17,7 +17,7 @@ public class CourseDAO {
     @Autowired
     Course course;
     int courseFeeAI = 55000;
-    int courseFeeML = 25077888
+    int courseFeeML = 2505555;
     public List<Course> getAllCourseDetails()
     {
         List<Course> courseList = new ArrayList<Course>();
